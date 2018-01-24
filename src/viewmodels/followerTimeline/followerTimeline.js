220bba7a25c94f49c0d53350ef993e1ae77e7705
Matthias <matthias.rudingsdorfer@gmail.com>
2018-01-24 16:53:39 +0100

    implemented deletion of single tweets, gui update, personal -> followersTimeline

---
+++ src/viewmodels/followerTimeline/followerTimeline.js
@@ -0,0 +1,49 @@
+import {inject} from 'aurelia-framework';
+import TwitterService from '../../services/twitter-service';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {TweetUpdate} from '../../services/messages';
+
+@inject(TwitterService, EventAggregator)
+export class PersonalTimeline {
+
+  tweets = [];
+  followedUsers = [];
+
+  constructor(ts, ea) {
+    this.twitterService = ts;
+    this.ea = ea;
+    this.updateTweets();
+    this.ea.subscribe(TweetUpdate, msg => {
+      console.log('personal subscribed');
+      this.updateTweets();
+    });
+  }
+
+  updateTweets() {
+    this.tweets = [];
+    for (let user of this.twitterService.users){
+      for (let follower of user.followers) {
+        if (follower._id === this.twitterService.ownUser._id) {
+          this.followedUsers.push(user);
+        }
+      }
+    }
+    for (let tweet of this.twitterService.tweets) {
+      if (tweet.tweeter._id === this.twitterService.ownUser._id) {
+        this.tweets.push(tweet);
+      }
+      else {
+        for (let user of this.followedUsers) {
+          if (tweet.tweeter._id === user._id) {
+            this.tweets.push(tweet);
+          }
+        }
+      }
+    }
+  }
+
+  attached() {
+    console.log('personalTimeline attached');
+    console.log(this.tweets);
+  }
+}
