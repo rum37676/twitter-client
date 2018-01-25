1a218322c397fb67e7825e66b1807b04d4225ddb
Matthias <matthias.rudingsdorfer@gmail.com>
2018-01-25 11:48:23 +0100

    implemented remberMe function on login screen

--- src/viewmodels/login/login.js
+++ src/viewmodels/login/login.js
@@ -6,16 +6,31 @@ import {EventAggregator} from 'aurelia-event-aggregator';
 @inject(EventAggregator, TwitterService)
 export class Login {
 
-  email = 'marge@simpson.com ';
-  password = 'secret';
+  email = '';
+  password = '';
   rememberMe = false;
 
   constructor(ea, ts) {
     this.twitterService = ts;
+    if (localStorage.emailTwitter !== 'null' && typeof localStorage.emailTwitter !== 'undefined') {
+      let loginOptions = JSON.parse(localStorage.emailTwitter);
+      this.email = loginOptions.email;
+      this.rememberMe = loginOptions.rememberMe;
+    }
   }
 
   login() {
     console.log(`Trying to log in ${this.email}`);
+    if (this.rememberMe === true) {
+      let loginOptions = {
+        email: this.email,
+        rememberMe: this.rememberMe
+      };
+      localStorage.emailTwitter = JSON.stringify(loginOptions);
+    }
+    if (this.rememberMe === false) {
+      localStorage.emailTwitter = null;
+    }
     this.twitterService.login(this.email.trim(), this.password.trim());
   }
 }
